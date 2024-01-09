0e02888ab80e4ef442e7febe0d21d355ab677fb7
LegBitsTM <ertyua90@gmail.com>
2024-01-09 13:01:50 +0700

    home

--- src/screen/Home/index.js
+++ src/screen/Home/index.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, Pressable, onPress } from "react-native";
 import styles from "./styles";
 
 export default class Home extends Component {
     render() {
         return(
-            <View style={styles.container}>
-                <Text style={styles.text}>Home Page</Text>
-            </View>
+            <SafeAreaView style={styles.container}>
+                <View style={styles.containerScore}>
+                    <Text style={styles.scoreValue}>100</Text>
+                    <View style={styles.subContainerScore}>
+                        <Text style={styles.scoreTitle}>Water Quality Score</Text>
+                        <Text style={styles.scoreGrade}>Excellent</Text>
+                    </View>
+                </View>
+                <View style={styles.containerSection}>
+                    <View>
+                    <Text style={styles.sectionTitle}>Sensor Insights</Text>
+                    <Text style={styles.sectionSubtitle}>Last sync’d: 10 minutes ago</Text>
+                    </View>
+                    <Pressable style={styles.button} onPress={onPress}>
+                        <Text style={styles.text}>Save</Text>
+                    </Pressable>
+                </View>
+                <ScrollView>
+                    <View style={styles.containerParams}>
+                        <Text style={styles.paramsTitle}>Water Quality</Text>
+                        <Text style={styles.paramsValue}>100</Text>
+                    </View>
+                    <View style={styles.containerParams}>
+                        <Text style={styles.paramsTitle}>Water Temperature</Text>
+                        <Text style={styles.paramsValue}>100</Text>
+                    </View>
+                    <View style={styles.containerParams}>
+                        <Text style={styles.paramsTitle}>Total Dissolved Solids</Text>
+                        <Text style={styles.paramsValue}>100</Text>
+                    </View>
+                    <View style={styles.containerParams}>
+                        <Text style={styles.paramsTitle}>Turbidity</Text>
+                        <Text style={styles.paramsValue}>100</Text>
+                    </View>
+                    <View style={styles.containerParams}>
+                        <Text style={styles.paramsTitle}>pH Level</Text>
+                        <Text style={styles.paramsValue}>100</Text>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
         )
     }
 }
--- src/screen/Home/styles.js
+++ src/screen/Home/styles.js
@@ -3,14 +3,93 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
-    text: {
+    containerScore: {
+        margin: 20,
+        padding: 20,
+        borderRadius: 16,
+        backgroundColor: '#348EF4',
+        flexDirection: 'row',
+        alignItems: 'center'
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    subContainerScore: {
+        paddingLeft: 20
+    },
+    scoreValue: {
         textAlign: 'center',
-        fontSize: 25,
+        fontSize: 36,
+        fontWeight: '500',
+        color: '#F5F5F5',
+        // borderWidth: 5,
+        // borderRadius: 100,
+        // padding: 10,
+        // borderColor: '#F5F5F5',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    scoreTitle: {
+        textAlign: 'left',
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#F5F5F5'
+    },
+    scoreGrade: {
+        textAlign: 'left',
+        fontSize: 32,
         fontWeight: '700',
-    }
+        color: '#F5F5F5'
+    },
+    syncBtn: {
+        backgroundColor: '#fff'
+    },
+    containerSection: {
+        paddingLeft: 20,
+        paddingBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center'
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    sectionTitle: {
+        textAlign: 'left',
+        fontSize: 20,
+        fontWeight: '700',
+        marginBottom: 5
+    },
+    sectionSubtitle: {
+        textAlign: 'left',
+        fontSize: 12,
+        fontWeight: '500',
+    },
+    containerParams: {
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 20,
+        padding: 20,
+        borderRadius: 16,
+        borderLeftWidth: 5,
+        borderLeftColor: '#2FBF71',
+        backgroundColor: '#FFF'
+        // alignItems: 'center',
+        // justifyContent: 'center',
+    },
+    paramsTitle: {
+        textAlign: 'left',
+        fontSize: 16,
+        fontWeight: '500',
+        marginBottom: 5
+    },
+    paramsValue: {
+        textAlign: 'left',
+        fontSize: 32,
+        fontWeight: '700',
+        marginBottom: 5
+    },
+    
 })
 
 export default styles;
